2ec1c79e48f89bc7d94ef9204a6c37cb3e33bbbb
GerdaSiitan <gerda.siitan13@gmail.com>
2023-11-29 15:33:11 +0200

    sdfdfdsf

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import _ from 'lodash';
+import './style.scss';
 
-function component() {
-    const element = document.createElement('div');
-  
+import * as bootstrap from 'bootstrap';
+import { createApp } from 'vue';
+import * as VueRouter from 'vue-router';
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+import App from './App.vue';
+import ToDo from './pages/ToDo.vue';
+import Home from './pages/Home.vue';
+import Clicker from './pages/Clicker.vue';
+import RickAndMorty from './pages/RickAndMorty.vue';
+import GoogleMaps from './pages/GoogleMaps.vue';
+
+const routes = [
+ {path: '/', component: Home},
+ {path: '/todo', component: ToDo},
+ {path: '/clicker', component: Clicker},
+ {path: '/rickandmorty', component: RickAndMorty},
+ {path: '/gmaps', component: GoogleMaps},
+];
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHistory(),
+    routes
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,64 @@
-const path = require('path');
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import glob from 'glob';
+import { PurgeCSSPlugin } from 'purgecss-webpack-plugin';
+import { VueLoaderPlugin } from 'vue-loader';
+import Dotenv from 'dotenv-webpack';
 
-module.exports = {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist')
   },
-};
-
-module.exports = {
-    mode: 'development',
-  };
+  devServer: {
+      static: {
+          directory: path.resolve(__dirname, 'public')
+      },
+      historyApiFallback: true,
+      open: true,
+      port: 9000,
+      compress: true,
+  },
+  module: {
+      rules: [
+          {
+              test: /\.css$/i,
+              use: [MiniCssExtractPlugin.loader,'css-loader']
+          },
+          {
+              test: /\.s[ac]ss$/i,
+              use: [MiniCssExtractPlugin.loader,'css-loader', 'sass-loader']
+          },
+          {
+              test: /\.nunjucks$/i,
+              use: ['simple-nunjucks-loader']
+          },
+          {
+              test: /\.vue$/,
+              loader: 'vue-loader'
+          }
+      ]
+  },
+  plugins: [
+      new HtmlWebpackPlugin({
+          template: './src/index.html'
+      }),
+      new HtmlWebpackPlugin({
+          filename: '200.html',
+          template: './src/index.html'
+      }),
+      new MiniCssExtractPlugin(),
+      // new PurgeCSSPlugin({
+      //     paths: glob.sync(`src/views/**/*`, { nodir: true }),
+      // }),
+      new VueLoaderPlugin(),
+      new Dotenv()
+  ],
+}
